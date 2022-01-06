59c506621f9abfd7f222a1b5699786a2424e4184
noahliechti <noah.liechti@gmail.com>
2022-01-06 19:56:19 +0100

    one file

--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ const path = require("path");
 const serverless = require("serverless-http");
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors");
-const createScreenshot = require("./create-screenshot");
+const puppeteer = require("puppeteer");
 
 const PORT = process.env.PORT || 3000;
 const HOST = "localhost";
@@ -21,6 +20,8 @@ const TWEET_HIDE_CARD = false;
 const app = express();
 
 app.use(express.json());
+
+// const cors = require("cors");
 // app.use(
 //   cors({
 //     origin: ["http://127.0.0.1:1234", "http://localhost:1234"],
@@ -83,6 +84,63 @@ app.post("/get-image", async (req, res) => {
 
   res.send(screenshot);
 });
+
+const createScreenshot = async (props) => {
+  try {
+    const { lang, width, theme, padding, hideCard, hideThread, tweetId } =
+      props;
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    const page = await browser.newPage();
+    await page.goto(
+      `https://platform.twitter.com/embed/index.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=${hideCard}&hideThread=${hideThread}&id=${tweetId}&lang=${lang}&theme=${theme}&widgetsVersion=ed20a2b%3A1601588405575`,
+      { waitUntil: "networkidle0" }
+    );
+
+    const embedDefaultWidth = 550;
+    const percent = width / embedDefaultWidth;
+    const pageWidth = embedDefaultWidth * percent;
+    const pageHeight = 100;
+    await page.setViewport({ width: pageWidth, height: pageHeight });
+
+    await page.evaluate(
+      (props) => {
+        const { theme, padding, percent } = props;
+
+        const style = document.createElement("style");
+        style.innerHTML =
+          "* { font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol' !important; }";
+        document.getElementsByTagName("head")[0].appendChild(style);
+
+        const body = document.querySelector("body");
+        body.style.padding = `${padding}px`;
+        body.style.backgroundColor = theme === "dark" ? "#000" : "#fff";
+        body.style.zoom = `${100 * percent}%`;
+        const articleWrapper = document.querySelector("#app > div");
+        articleWrapper.style.border = "none";
+      },
+      { theme, padding, percent }
+    );
+
+    const imageBuffer = await page.screenshot({
+      type: "png",
+      fullPage: true,
+      encoding: "base64",
+    });
+
+    await browser.close();
+
+    console.log(imageBuffer);
+
+    return imageBuffer;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 if (process.env.NODE_ENV === "DEV") {
   app.listen(PORT, () => {
