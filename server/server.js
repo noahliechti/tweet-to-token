bbebf3039561cbfa881651d0a97a65dde85ac779
noahliechti <noah.liechti@gmail.com>
2022-01-06 18:27:50 +0100

    no listening

--- server/server.js
+++ server/server.js
@@ -29,9 +29,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${HOST}:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${HOST}:${PORT}`);
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
