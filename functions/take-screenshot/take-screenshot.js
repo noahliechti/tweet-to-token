f1dff740d123e10da3ff32b0d8d1000f6a563756
noahliechti <noah.liechti@gmail.com>
2022-01-08 02:52:51 +0100

    CHANGE response

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -7,62 +7,59 @@ const TWEET_HIDE_CARD = false;
 
 exports.handler = async (event, context) => {
   const { tweetURL, theme, lang } = JSON.parse(event.body);
-  try {
-    const browser = await chromium.puppeteer.launch({
-      executablePath: await chromium.executablePath,
-    });
 
-    const page = await browser.newPage();
-    await page.goto(
-      `https://platform.twitter.com/embed/index.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=${TWEET_HIDE_CARD}&hideThread=${TWEET_HIDE_THREAD}&id=${getTweetId(
-        tweetURL
-      )}&lang=${lang}&theme=${theme}&widgetsVersion=ed20a2b%3A1601588405575`,
-      { waitUntil: "networkidle0" }
-    );
+  const browser = await chromium.puppeteer.launch({
+    executablePath: await chromium.executablePath,
+  });
 
-    const embedDefaultWidth = 550;
-    const percent = TWEET_WIDTH / embedDefaultWidth;
-    const pageWidth = embedDefaultWidth * percent;
-    const pageHeight = 100;
-    await page.setViewport({ width: pageWidth, height: pageHeight });
+  const page = await browser.newPage();
+  await page.goto(
+    `https://platform.twitter.com/embed/index.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=${TWEET_HIDE_CARD}&hideThread=${TWEET_HIDE_THREAD}&id=${getTweetId(
+      tweetURL
+    )}&lang=${lang}&theme=${theme}&widgetsVersion=ed20a2b%3A1601588405575`,
+    { waitUntil: "networkidle0" }
+  );
 
-    await page.evaluate(
-      (props) => {
-        // const { theme, padding, percent } = props;
+  const embedDefaultWidth = 550;
+  const percent = TWEET_WIDTH / embedDefaultWidth;
+  const pageWidth = embedDefaultWidth * percent;
+  const pageHeight = 100;
+  await page.setViewport({ width: pageWidth, height: pageHeight });
 
-        const style = document.createElement("style");
-        style.innerHTML =
-          "* { font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol' !important; }";
-        document.getElementsByTagName("head")[0].appendChild(style);
+  await page.evaluate(
+    (props) => {
+      const { theme, padding, percent } = props;
 
-        const body = document.querySelector("body");
-        body.style.padding = `${TWEET_PADDING}px`;
-        body.style.backgroundColor = theme === "dark" ? "#000" : "#fff";
-        body.style.zoom = `${100 * percent}%`;
-        const articleWrapper = document.querySelector("#app > div");
-        articleWrapper.style.border = "none";
-      },
-      { theme, padding, percent }
-    );
+      const style = document.createElement("style");
+      style.innerHTML =
+        "* { font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol' !important; }";
+      document.getElementsByTagName("head")[0].appendChild(style);
 
-    const imageBuffer = await page.screenshot({
-      type: "png",
-      fullPage: true,
-      encoding: "binary",
-    });
+      const body = document.querySelector("body");
+      body.style.padding = `${padding}px`;
+      body.style.backgroundColor = theme === "dark" ? "#000" : "#fff";
+      body.style.zoom = `${100 * percent}%`;
+      const articleWrapper = document.querySelector("#app > div");
+      articleWrapper.style.border = "none";
+    },
+    { theme, padding, percent }
+  );
 
-    await browser.close();
+  const imageBuffer = await page.screenshot({
+    type: "png",
+    fullPage: true,
+    encoding: "base64",
+  });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `Complete screenshot`,
-        buffer: imageBuffer,
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-  }
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Complete screenshot`,
+      buffer: imageBuffer,
+    }),
+  };
 };
 
 function getTweetId(tweetURL) {
