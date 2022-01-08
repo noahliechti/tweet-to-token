f047d45e9aa6af7a8d144a55462c7da5f5ef2587
noahliechti <noah.liechti@gmail.com>
2022-01-08 02:33:01 +0100

    CHANGE response

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      buffer: screenshot,
+      message: screenshot,
     }),
   };
 };
