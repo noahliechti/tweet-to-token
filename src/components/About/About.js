d44f1d16864fdebc4c76674fb06d2a995995b2ae
noahliechti <noah.liechti@gmail.com>
2022-01-17 18:22:22 +0100

    fix text alignment bug

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Box } from "@mui/material";
 
 import { ReactComponent as AskGraphic } from "../../assets/graphics/company.svg";
 
@@ -9,21 +9,24 @@ function About() {
         <AskGraphic />
       </Grid>
       <Grid item xs={12} md={6}>
-        <Typography sx={{ textAlign: { xs: "center", md: "left" } }}>
-          <em>tweettoken.io</em> is a platform built on Ethereum. It let's you
-          mint your Tweets with a few clicks <strong>for free</strong>. We offer
-          <strong> customization options</strong> for the theme and the language
-          of the tweet.
-        </Typography>
-        <br />
-        <Typography>
-          NFT Tweets can be sold for <strong>millions of dollars</strong> and we
-          think everybody should be able to participate in this market.
-        </Typography>
-        <Typography>
-          <em>tweettoken.io</em> provides the easiest and fastest way to mint
-          <strong> metadata-rich</strong> Tweet NFTs!
-        </Typography>
+        <Box sx={{ textAlign: { xs: "center", md: "left" } }}>
+          <Typography>
+            <em>tweettoken.io</em> is a platform built on Ethereum. It let's you
+            mint your Tweets with a few clicks <strong>for free</strong>. We
+            offer
+            <strong> customization options</strong> for the theme and the
+            language of the tweet.
+          </Typography>
+          <br />
+          <Typography>
+            NFT Tweets can be sold for <strong>millions of dollars</strong> and
+            we think everybody should be able to participate in this market.
+          </Typography>
+          <Typography>
+            <em>tweettoken.io</em> provides the easiest and fastest way to mint
+            <strong> metadata-rich</strong> Tweet NFTs!
+          </Typography>
+        </Box>
       </Grid>
     </Grid>
   );
