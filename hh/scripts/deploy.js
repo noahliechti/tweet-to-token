3e1a08916dffeb01bb08bb8b54afaa092cb5f1dd
noahliechti <noah.liechti@gmail.com>
2022-01-30 14:36:22 +0100

    write deploy script

--- hh/scripts/deploy.js
+++ hh/scripts/deploy.js
@@ -1,32 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { storeContractAddress } = require("./helper-function");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const ethers = hre.ethers;
 
-  // We get the contract to deploy
-  const TTT = await hre.ethers.getContractFactory("TweetToken");
+  const contractName = "TweetToken";
+  const TTT = await ethers.getContractFactory(contractName);
   const ttt = await TTT.deploy("https://gateway.pinata.cloud/ipfs/");
 
   await ttt.deployed();
+  storeContractAddress(ttt, contractName);
 
-  console.log("Deployer:", (await hre.ethers.getSigners())[0].address);
+  console.log("Deployer:", (await ethers.getSigners())[0].address);
   console.log("TweetToken deployed to:", ttt.address);
 
-  // TODO: publish source, from which account is deployed?
+  // TODO: publish source
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
